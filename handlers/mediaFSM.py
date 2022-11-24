c4f5aca7eafc47da32fe70a03d1db8a3aa723273
TechnoKotan <fendaroadz@ya.ru>
2022-11-24 23:36:31 +0300

    Some refractorint in mediaFSM and sqlite (still doesn't work tho)

--- handlers/mediaFSM.py
+++ handlers/mediaFSM.py
@@ -6,16 +6,19 @@ from keyboards import get_main_kb, get_which_instrument_kb, get_yes_no_kb
 from media_db import sqlite
 
 
+
+
 class FSMMedia(StatesGroup):
-    which_instrument = State()  # state 0, answer which instrument
+    which_instrument = State()
     send_video = State()
 
-# Starting FSM 
 
+# Starting FSM
 
 async def media_fsm(message: types.Message):  # MEDIA command, no state yet
     await message.delete()
     await FSMMedia.which_instrument.set()
+    # Asking 1st question and giving specified keyboard with answers.
     await message.answer("Which instrument do you wanna see me playing?", reply_markup=get_which_instrument_kb())
 
 
@@ -30,56 +33,32 @@ async def cancel_handler(message: types.Message, state=FSMContext):  # CANCEL co
 
 
 async def which_instrument(message: types.Message, state=FSMContext):
-    async with state.proxy() as data:
+    async with state.proxy() as data:  # saving answer from the user into the state object of FSM
         data["instrument"] = message.text.lower()[1:-1]
-    await FSMMedia.next()
-    await message.answer(f"This is video of me playing whatever instrument you have chosen", reply_markup=get_yes_no_kb())
+        await FSMMedia.next()
+    global list_of_id
+    list_of_id = sqlite.get_videos_from_db(data["instrument"])
+    # and sending first video of the chosen category
+    await message.answer_video(next(sqlite.yield_id(list_of_id)), reply_markup=get_yes_no_kb())
     await message.answer("Wanna some more vids?")
 
 
 async def send_video(message: types.Message, state=FSMContext):
-    print("We have entered to send_video state")
+
+    # Asking user if he wants one more video, and if yes, sending next video from db, until the answer is no
+    # or if StopIteration happens...
+
+    print("We have entered to send_video state")  # some debugging msg
+
     if message.text != "✔️YES✔️":
-        await state.finish()
+        await state.finish()    # exiting from FSM
         await message.answer("Ok, getting back to main menu", reply_markup=get_main_kb())
-        i = 0
         return
-    async with state.proxy() as data:
-        await message.answer_video(next(sqlite.get_videos_from_db(data["instrument"])))
+
+    await message.answer_video(next(sqlite.yield_id(list_of_id)), reply_markup=get_yes_no_kb())
+    # Setting FSM to present state again, and waiting if user answers no, or there will be no more content in database
     await state.set_state(FSMMedia.send_video)
     await message.answer("Wanna MOAR?", reply_markup=get_yes_no_kb())
-
-    # await message.answer_video(videos[0])
-
-    # await message.answer("There is no more video yet... 😢")
-    # await state.finish()
-
-# async def send_video(message: types.Message, state = FSMContext):
-#     await message.answer("Wanna MOAR?", reply_markup=get_yes_no_kb())
-#     if message.text != "✔️YES✔️":
-
-
-# async def send_video(message: types.Message, state: FSMContext): #SONG list in specific genre GENRE
-#     async with state.proxy() as data:
-#         data["song"] = message.text
-#     await FSMMedia.next()
-#     # send_video method calling here!
-#     try:
-#         await message.answer_video(sqlite.get_video(data["song"]), reply_markup=get_main_kb())
-#         await message.answer("Here we go!")
-#     except (BadRequest, TypeError):
-#         await message.answer("There is no video yet 😢", reply_markup=get_main_kb())
-#     await state.finish()
-
-# async def select_genre(message: types.Message, state: FSMContext): # GENRE list
-#     async with state.proxy() as data:
-#         data["genre"] = message.text
-#     question = "Which song do you like to hear?"
-#     await FSMMedia.next()
-#     if data["genre"] == "🎸METAL🎸":
-#         await message.answer(question, reply_markup=get_metal_songs())
-#     if data["genre"] == "🎸FUSION🎸":
-#         await message.answer(question, reply_markup=get_fusion_songs())
 
 
 def register_command_FSM(dp: Dispatcher):
@@ -87,9 +66,7 @@ def register_command_FSM(dp: Dispatcher):
         equals="MEDIA", ignore_case=True), state=None)
     dp.register_message_handler(cancel_handler, Text(
         equals="🔇CANCEL🔇", ignore_case=True), state="*")  # here cancel button
-    dp.register_message_handler(
-        which_instrument, state=FSMMedia.which_instrument)
-
+    dp.register_message_handler(which_instrument, state=FSMMedia.which_instrument)
     # dp.register_message_handler(select_genre, state=FSMMedia.genre)
     # dp.register_message_handler(send_video, state=FSMMedia.send_media)
     dp.register_message_handler(send_video, state=FSMMedia.send_video)
--- media_db/sqlite.py
+++ media_db/sqlite.py
@@ -1,23 +1,30 @@
 import sqlite3
 # from media_db import video_commands
 
+
 def connect_to_db():
     global cur, base
     base = sqlite3.connect("kotan_media.db")
     cur = base.cursor()
 
-    base.execute("CREATE TABLE IF NOT EXISTS bass(filename PRIMARY KEY, file_id)")
+    base.execute(
+        "CREATE TABLE IF NOT EXISTS bass(filename PRIMARY KEY, file_id)")
     base.commit()
-    base.execute("CREATE TABLE IF NOT EXISTS keys(filename PRIMARY KEY, file_id)")
+    base.execute(
+        "CREATE TABLE IF NOT EXISTS keys(filename PRIMARY KEY, file_id)")
     base.commit()
-    base.execute("CREATE TABLE IF NOT EXISTS guitar(filename PRIMARY KEY, file_id)")
+    base.execute(
+        "CREATE TABLE IF NOT EXISTS guitar(filename PRIMARY KEY, file_id)")
     base.commit()
-    base.execute("CREATE TABLE IF NOT EXISTS techno(filename PRIMARY KEY, file_id)")
+    base.execute(
+        "CREATE TABLE IF NOT EXISTS techno(filename PRIMARY KEY, file_id)")
     base.commit()
     if base:
-        print ("Sucsessfully connected to kotan_media.db")
+        print("Sucsessfully connected to kotan_media.db")
 
-# adding video to db
+# adding video to db (only admin)
+
+
 async def insert_video(raw_data):
     async with raw_data.proxy() as data:
         category = data["category"]
@@ -25,28 +32,13 @@ async def insert_video(raw_data):
         cur.execute(f"INSERT INTO {category} VALUES (?, ?)", values)
         base.commit()
 
-
-# sending video to client
-# async def get_video(name, category):
-#         for key in video_commands.video_dict:
-#             if key == name:
-#                 return cur.execute(f"SELECT file_id FROM {category} WHERE filename == ?", (video_commands.video_dict[key],)).fetchone()[0]
-
-
+# code below doesn't work properly yet :(
 
 
 def get_videos_from_db(category):
-    id_list = list((i[0] for i in cur.execute(f"SELECT file_id FROM {category}")))
-    for i in id_list:
+    return list((i[0] for i in cur.execute(f"SELECT file_id FROM {category}")))
+
+
+def yield_id(list_of_id):
+    for i in list_of_id:
         yield i
-
- 
-    
- 
-# connect_to_db()
-# print (get_video("🤘MESHUGGAH🤘"))
-# print (video_dict)
-
-# make_json()
-
-# print (get_videos_from_db("bass"))
